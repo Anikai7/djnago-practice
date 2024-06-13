4c8b40f75b51fc8378b13596c59c5af1f01a58b8
Anikait7 <anikait749@gmail.com>
2024-06-13 17:54:23 +0530

    added forms

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('login',views.login,name="login"),
+    path('home',views.home,name="home"),
+    path('contact',views.contact,name="contact"),
+    path('details',views.details,name="details")
+
 ]
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,6 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def home(request):
+    return render(request,"home.html")
+
+def contact(request):
+    return render(request,"form.html")
+
+def details(request):
+    mydictonary={
+        "mail" : request.POST["exampleFormControlInput1"],
+        "message": request.POST['exampleFormControlTextarea1'],
+        "method": request.method
+    }
+    return JsonResponse( mydictonary)
+
+
